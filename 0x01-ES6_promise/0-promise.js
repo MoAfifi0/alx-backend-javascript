7cdca628bde815d22faec6f3ef5607f47ba9a681
MoAfifi0 <m.afifi00000gmail.com>
2023-07-26 11:20:35 +0200

    fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
 export default function getResponseFromAPI() {
-  return new Promise(() => {});
+	const promise = new Promise((reselve, reject) => {
+		resolve();
+		reject();
+	});
+  	return promise;
 }
